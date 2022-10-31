68b0055237565c3afc7ff98ebeb04f8ba8ef3734
Salaheddin El Hamraoui <salah.infospace@gmail.com>
2022-10-31 11:42:34 +0100

    add affiliate screen

---
+++ screens/affiliate/Affiliate.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Affiliate = () => {
+    return (
+        <View>
+            <Text>Affiliate</Text>
+        </View>
+    )
+}
+
+export default Affiliate
